2460bd3f730228ed82fb321ddea9e25891582106
thi-costa <thiago14abr@gmail.com>
2022-04-11 08:38:03 -0300

    Update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import envVariables from './config/env';
 
+console.log('Teste');
 async function bootstrap() {
+  const logger = new Logger('NestApplication');
+  console.log('Teste');
+  logger.log('Teste');
+
+  console.log(envVariables());
+  console.log('Teste: ', process.env.db_password);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  logger.log(process.env.PORT);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
