257ef1234ff74d2641d0b6f5005ce060b4dcf102
thi-costa <thiago14abr@gmail.com>
2022-04-11 08:39:27 -0300

    Include env variables and configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import ormconfig from '../ormconfig';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import envConfig from './config/env';
 
-export function DatabaseOrmModule(): DynamicModule {
-  return TypeOrmModule.forRoot(ormconfig);
-}
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig)],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfig],
+      isGlobal: true,
+    }),
+    // PostgresProviderModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
