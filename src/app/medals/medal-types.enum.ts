8db398e565396b34a999f804e689446dc9aeb94b
thi-costa <thiago14abr@gmail.com>
2022-04-11 08:39:50 -0300

    Enum medal types

---
+++ src/app/medals/medal-types.enum.ts
@@ -0,0 +1,10 @@
+export enum MedalTypes {
+  DIAMOND = 'diamond',
+  RUBY = 'ruby',
+  EMERALD = 'emerald',
+  SAPHIRE = 'saphire',
+  PLATINUM = 'platinum',
+  GOLD = 'gold',
+  SILVER = 'silver',
+  BRONZE = 'bronze',
+}
