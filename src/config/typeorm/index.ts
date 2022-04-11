8e3bb18bf362a0cff1136751c6c541fae9ae896b
thi-costa <thiago14abr@gmail.com>
2022-04-11 08:42:44 -0300

    Create env variables typeorm

---
+++ src/config/typeorm/index.ts
@@ -0,0 +1,33 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+interface configByEnvironment {
+  prd?: TypeOrmModuleOptions;
+  local?: TypeOrmModuleOptions;
+  development?: TypeOrmModuleOptions;
+  staging?: TypeOrmModuleOptions;
+}
+
+export default (configService: ConfigService): TypeOrmModuleOptions => {
+  const environment = configService.get<string>('nodeEnv', 'local');
+
+  const environments: configByEnvironment = {
+    local: {
+      host: configService.get<string>('dbHost', 'localhost'),
+      port: configService.get<number>('dbPort', 5432),
+      username: configService.get<string>('dbUser', 'postgres'),
+      password: configService.get<string>('dbPassword', '14040315'),
+      database: configService.get<string>('dbName', 'pharus-db'),
+      autoLoadEntities: true,
+      entities: [`${__dirname}/dist/shared/entities/**/*.entity.{js|ts}`],
+    },
+  };
+
+  const config = environments[environment];
+  console.log(config);
+
+  return {
+    type: 'postgres',
+    ...config,
+  };
+};
